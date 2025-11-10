60fa6555016b01af34f77ebd5733090f9e3ea1ea
Giobw <gmebencini@gmail.com>
2025-11-10 18:12:47 -0300

    Version inicial del proyecto Salmontt

---
+++ src/model/Direccion.java
@@ -0,0 +1,51 @@
+package model;
+
+/**
+ * Clase que representa una Dirección.
+ * Se utiliza para demostrar la composición (una Persona "tiene una" Dirección).
+ */
+public class Direccion {
+
+    // Atributos encapsulados (private) [cite: 43]
+    private String calle;
+    private String numero;
+    private String ciudad;
+
+    // Constructor para inicializar el objeto [cite: 46]
+    public Direccion(String calle, String numero, String ciudad) {
+        this.calle = calle;
+        this.numero = numero;
+        this.ciudad = ciudad;
+    }
+
+    // Métodos Getters y Setters (para acceder a los atributos) [cite: 44]
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    // Método toString() para mostrar el objeto [cite: 45]
+    @Override
+    public String toString() {
+        return calle + " #" + numero + ", " + ciudad;
+    }
+}
---
+++ src/model/Persona.java
@@ -0,0 +1,65 @@
+package model;
+
+/**
+ * Clase Base que representa una Persona.
+ * Contiene un objeto Direccion (Composición).
+ */
+public class Persona {
+
+    // Atributos encapsulados (private) [cite: 43]
+    private String rut;
+    private String nombre;
+    private String apellido;
+    private Direccion direccion; // Composición
+
+    // Constructor [cite: 46]
+    public Persona(String rut, String nombre, String apellido, Direccion direccion) {
+        this.rut = rut;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.direccion = direccion;
+    }
+
+    // Getters y Setters [cite: 44]
+    public String getRut() {
+        return rut;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Direccion getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(Direccion direccion) {
+        this.direccion = direccion;
+    }
+
+    // Método toString() [cite: 45]
+    @Override
+    public String toString() {
+        return "Persona [" +
+                "rut='" + rut + '\'' +
+                ", nombre='" + nombre + ' ' + apellido + '\'' +
+                ", direccion=" + direccion.toString() + // Llama al toString() de Direccion
+                ']';
+    }
+}
